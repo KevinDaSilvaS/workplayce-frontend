85efc5bc90eabb49867c99b982355ee9c47123c2
Your Name <kevin.kds80@gmail.com>
2022-08-26 20:26:50 -0300

    add footer and header

---
+++ js/footer.js
@@ -0,0 +1,24 @@
+const footer = `
+    <footer class="page-footer purple darken-1 footer">
+        <div class="row container">
+            <h5 class="center">Workplayce</h5>
+            <p class="center">Ajude a financiar o projeto:</p>
+            <div class="col s4 m4 g4 center"></div>
+            <div class="col s2 m2 g2 center">
+                <a class="pink-text text-lighten-4" href="https://www.buymeacoffee.com/kevinworkplayce">Buy me a coffee</a>
+            </div>
+            <div class="col s2 m2 g2 center">
+                <a class="pink-text text-lighten-4" href="https://www.patreon.com/kevindasilva">Através do Patreon</a>
+            </div>
+            <div class="col s4 m4 g4 center"></div>
+        </div> 
+        <div class="footer-copyright">
+            <div class="container">
+            © 2021 Copyright
+            <a class="grey-text text-lighten-4 right" href="kevin.kds80@gmail.com">Contato</a>
+            </div>
+        </div>
+    </footer>
+`;
+
+document.body.innerHTML += footer;
---
+++ js/header.js
@@ -0,0 +1,11 @@
+const header = `
+    <header class="page-topbar row purple darken-1 z-depth-1 section section-left-space section-bottom-space">
+        <div class="navbar navbar-fixed"> 
+            <div class="col s1 m1 g1 logo-workplayce">
+                <img class="logo responsive-img " src="./img/logo.png" alt="Workplayce logo">
+            </div>
+        </div>
+    </header>
+`;
+
+document.body.innerHTML += header;
