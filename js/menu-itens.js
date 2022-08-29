ac88b99b910937a22c1b9ac3c5d64fedffec7d4c
Your Name <kevin.kds80@gmail.com>
2022-08-29 19:20:24 -0300

    add menu creation

---
+++ js/menu-itens.js
@@ -0,0 +1,44 @@
+const userPages = {
+    bookings: {
+        title: "Bookings",
+        link: "base-page.html?page=bookings",
+        icon: "bookmarks"
+    },
+    places: {
+        title: "Escritórios",
+        link: "base-page.html?page=places",
+        icon: "bookmarks"
+    }
+};
+
+const companyPages = {
+    places: {
+        title: "Meus Escritórios",
+        link: "base-page.html?page=places",
+        icon: "bookmarks"
+    },
+    bookings: {
+        title: "Solicitações de Bookings",
+        link: "base-page.html?page=bookings",
+        icon: "bookmarks"
+    }
+};
+
+const pages = {
+    "USER": userPages,
+    "COMPANY": companyPages
+}
+
+const setMenuItens = () => {
+    const user_type = getProperty("user_type");
+    const menuPages = pages[user_type];
+    const menuItensKeys = Object.keys(menuPages); 
+
+    const menu = menuItensKeys.map(page => {
+        const {title, link, icon} = menuPages[page];
+        return `<li class="bold" id="${page}"><a class="waves-effect waves-cyan" href="${link}"><i class="material-icons">${icon}</i><span class="menu-title">${title}</span></a>
+        </li>`
+    })
+    
+    document.getElementById("menu").innerHTML = menu
+};
