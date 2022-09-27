e6f022292319fc8fc0c981c2e37b23caa1f8bb77
Kevin da Silva <kevin.kds80@gmail.com>
2022-09-27 16:27:09 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const backend_host = "https://workplayce.fly.dev/api";//"http://localhost:4000/api";
-const url_login = "https://workplayce.fly.dev";
+const backend_host_login = "https://workplayce.fly.dev";
